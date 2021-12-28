46ceaffe1897f5dd3db83e6aa9b68c9b3f6d25bd
Lemontea <lemontea.Tom@gmail.com>
2021-12-28 09:09:25 +0800

    styling (accordian)

--- src/pages/CourseDetails.js
+++ src/pages/CourseDetails.js
@@ -10,6 +10,18 @@ import { Heading, Text } from '@chakra-ui/react';
 
 import { Steps, Step } from 'chakra-ui-steps';
 
+import { FaBook } from 'react-icons/fa';
+import { ImLab } from 'react-icons/im';
+import { BsQuestion } from 'react-icons/bs';
+
+import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+  } from '@chakra-ui/react'
+
 const readFromIPFS = async (path) => {
     const client = create({
         host: "ipfs.infura.io",
@@ -46,6 +58,16 @@ export default function CourseDetails() {
         fn();
     }, [params.ipfscid]);
 
+    const displayIcon = (type) => {
+        if (type == "reading_material") {
+            return FaBook;
+        } else if (type == "quiz") {
+            return BsQuestion;
+        } else if (type == "lab") {
+            return ImLab;
+        }
+    }
+
     return (
         <Box>
             <Box>Testing {params.ipfscid}</Box>
@@ -54,7 +76,31 @@ export default function CourseDetails() {
             <Text>{courseInfo["course_desc"]}</Text>
             <Heading as='h4'>Learning Outcome:</Heading>
             <Text>{courseInfo["course_obj"]}</Text>
-            <Steps><Step></Step></Steps>
+            <Accordion defaultIndex={[0]} allowMultiple>
+            {courseInfo["table_of_content"].map((section) => {
+                return (
+                    <AccordionItem>
+                        <h2>
+                        <AccordionButton>
+                            <Box flex='1' textAlign='left'>
+                            {section.title}
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                        </h2>
+                        <AccordionPanel pb={4}>
+                            <Steps orientation="vertical">
+                                {section.units.map((unit) => {
+                                    return (
+                                        <Step label={unit.title} icon={displayIcon(unit.types)}></Step>
+                                    )
+                                })}
+                            </Steps>
+                        </AccordionPanel>
+                    </AccordionItem>
+                    )
+            })}
+            </Accordion>
         </Box>
     );
 }
