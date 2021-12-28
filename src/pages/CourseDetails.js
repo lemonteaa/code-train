e52dd4d12acd0b5131b0da58cf9b004b4c2cfa44
Lemontea <lemontea.Tom@gmail.com>
2021-12-28 08:04:53 +0800

    display course details (I)

--- src/pages/CourseDetails.js
+++ src/pages/CourseDetails.js
@@ -2,9 +2,56 @@ import { Box } from "@chakra-ui/layout";
 
 import { useParams } from "react-router-dom";
 
+import { create } from "ipfs-http-client";
+
+import { useEffect, useState } from 'react';
+
+import { Heading, Text } from '@chakra-ui/react';
+
+const readFromIPFS = async (path) => {
+    const client = create({
+        host: "ipfs.infura.io",
+        port: 5001,
+        protocol: "https"
+    });
+
+    const res = [];
+    const data = await client.cat(path);
+    for await (let byte of data) {
+        const thisRound = Array.from(byte)
+        .map((x) => {
+            return String.fromCharCode(x);
+        })
+        .join("");
+        res.push(thisRound);
+    }
+    return res.join("");
+    //console.log(String.fromCharCode(72));
+}
+
 export default function CourseDetails() {
     let params = useParams();
+
+    const [courseInfo, setCourseInfo] = useState({});
+
+    useEffect( () => {
+        const fn = async () => {
+            console.log(params.ipfscid);
+            let rawManifest = await readFromIPFS("/ipfs/" + params.ipfscid + "/manifest.json");
+            console.log(rawManifest);
+            setCourseInfo(JSON.parse(rawManifest));
+        };
+        fn();
+    }, [params.ipfscid]);
+
     return (
-        <Box>Testing {params.ipfscid}</Box>
+        <Box>
+            <Box>Testing {params.ipfscid}</Box>
+            <Heading size='xl'>{courseInfo["course_title"]}</Heading>
+            <Heading as='h4'>Course Description:</Heading>
+            <Text>{courseInfo["course_desc"]}</Text>
+            <Heading as='h4'>Learning Outcome:</Heading>
+            <Text>{courseInfo["course_obj"]}</Text>
+        </Box>
     );
 }
